71ecd074b5238de7afea3b32938d29804bb3e083
Najib <najib.muqeem@gmail.com>
2020-02-12 18:02:00 +0000

    implemented module to handle inputs

--- play.js
+++ play.js
@@ -1,20 +1,7 @@
 const { connect } = require("./client");
+const { setupInput } = require("./input");
+
 console.log("Connecting ...");
 connect();
-
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-
-  stdin.on("data", key => {
-    if (key === "\u0003") {
-      process.exit();
-    }
-  });
-
-  return stdin;
-};
 
 setupInput();
