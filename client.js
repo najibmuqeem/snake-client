090899bc36d00aa55e2c7407250b63de3a5cb3eb
Najib <najib.muqeem@gmail.com>
2020-02-12 17:30:27 +0000

    sent and received some data to and from server

--- client.js
+++ client.js
@@ -8,11 +8,16 @@ const connect = function() {
     host: "localhost",
     port: 50541
   });
-  // interpret incoming data as text
+
   conn.setEncoding("utf8");
 
   conn.on("data", data => {
     console.log(data);
+  });
+
+  conn.on("connect", () => {
+    console.log("Successfully connected to game server!");
+    conn.write("Name: NMM");
   });
   return conn;
 };
